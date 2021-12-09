c55c0471a10ff133ced9a6b32d40b90e2ef3adfe
joshyeram <joshyc423@gmail.com>
2021-12-08 22:47:59 -0500

    s

--- distrib/kalman2d.py
+++ distrib/kalman2d.py
@@ -15,10 +15,18 @@ if __name__ == "__main__":
     lines = [line.rstrip('\n') for line in open(filename)]
     data = []
     for line in range(0, len(lines)):
-        data.append(map(float, lines[line].split(' ')))
+        temp = lines[line].split(' ')
+        data.append((float(temp[0]),float(temp[1]),float(temp[2]),float(temp[3])))
 
     # Print out the data
     print("The input data points in the format of 'k [u1, u2, z1, z2]', are:")
     for it in range(0, len(data)):
         print(str(it + 1) + ": ", end='')
         print(*data[it])
+
+    control = []
+    measurement = []
+    for i in data:
+        control.append((i[0],i[1]))
+        measurement.append((i[2], i[3]))
+    
