788df7a82b0bcaf25cbb2a7335bca4b22b68f9da
joshyeram <joshyc423@gmail.com>
2021-12-09 17:06:45 -0500

    working

--- distrib/kalman2d.py
+++ distrib/kalman2d.py
@@ -1,5 +1,7 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import inv
 
 def drawMeasurement(coords):
     if (coords == None):
@@ -10,6 +12,7 @@ def drawMeasurement(coords):
         plt.plot(x, y, 'yo', linestyle='solid', markersize=1)
     plt.show(block=False)
     return
+
 def drawCorrected(coords):
     if(coords == None):
         return
@@ -35,13 +38,18 @@ def draw(measurement, corrected):
     plt.show()
     return
 
-def do(control, measurement, init, scaler):
-    x0 = init[0]
-    y0 = init[1]
-    Q = None
-    R = None
+def kalmanFilterTimeUpdate(x, A, B, u, P, Q):
+    xTemp = np.dot(A, x) + np.dot(B, u)
+    pTemp = np.dot(A, np.dot(P, np.transpose(A))) + Q
+    return xTemp, pTemp
 
-    return
+def kalmanFilterMeasurementUpdate(P, H, R, x, z, l):
+    temp = np.dot(H, np.dot(P, np.transpose(H))) + R
+    inverse = inv(temp)
+    K = np.dot(P, np.dot(np.transpose(H), inverse))
+    xTemp = x + np.dot(K, (z - np.dot(H, x)))
+    pTemp = np.dot(((np.identity(2)*l) - np.dot(K, H)), P)
+    return xTemp, pTemp
 
 def guess(control, init):
     x0 = init[0]
@@ -54,6 +62,7 @@ def guess(control, init):
     return location
 
 if __name__ == "__main__":
+    np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
     # Retrive file name for input data
     if (len(sys.argv) < 5):
         print("Four arguments required: python kalman2d.py [datafile] [x1] [x2] [lambda]")
@@ -86,8 +95,24 @@ if __name__ == "__main__":
     print(control)
     print(measurement)
 
-    measurement.insert(0, (0,0))
-    g = guess(control, (x0, y0))
-    g.insert(0,(0,0))
-    draw(measurement, g)
+    Q = np.array([[0.0001, 0.00002], [0.00002, 0.0001]])
+    R = np.array([[0.01, 0.005], [0.005, 0.02]])
+
+    updated = []
+    coords = None
+    P = np.identity(2)
+    x = np.array([[x0], [y0]], dtype=object)
+    H = np.identity(2)
+    A = np.identity(2)
+    B = np.identity(2)
+    for i in range(len(data)):
+        u = np.array([[data[i][0]],[data[i][1]]])
+        (x, P) = kalmanFilterTimeUpdate(x, A, B, u, P, Q)
+        z = np.array([[data[i][2]], [data[i][3]]])
+        (x, P) = kalmanFilterMeasurementUpdate(P, H, R, x, z, scaler)
+        updated.append(x)
+
+    measurement.insert(0, (x0, y0))
+    updated.insert(0, (x0, y0))
+    draw(measurement, updated)
     
