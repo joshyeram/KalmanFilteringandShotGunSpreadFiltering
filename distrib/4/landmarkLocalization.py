f276b0ced6407ab854f8aeaea939fe7873088075
joshyeram <joshyc423@gmail.com>
2021-12-12 04:05:07 -0500

    4

--- distrib/4/landmarkLocalization.py
+++ distrib/4/landmarkLocalization.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import inv
+import math
 
 robot = (4,8)
 
@@ -60,6 +61,44 @@ def readGroundTruth(fileName):
         pos.append((float(s[0]), float(s[1]), float(s[2])))
     return np.array(pos)
 
+def sample(enviro, groundTruth):
+    env = readEnv(enviro)
+    gt = readGroundTruth(groundTruth)
+    tempMake = []
+    for i in range(len(gt)-1):
+        diff = np.subtract(gt[i+1], gt[i])
+        trans = np.sqrt(diff[0]**2 + diff[1]**2)
+        rot = diff[2]
+        sT = np.random.normal(trans, .1)
+        sR = np.random.normal(rot, .1)
+        sR2 = np.random.normal(rot, .1)
+        tempMake.append((sR, sT, sR2))
+    tempObs = []
+    for i in range(len(gt) - 1):
+        tempB = []
+        for j in env:
+            b = math.atan2(j[1]-gt[i][1], j[0]-gt[i][0]) - gt[i][2]
+            sB = np.random.normal(b, .0523)
+            while(sB<0):
+                sB += np.pi*2
+            sB %= np.pi*2
+            tempB.append(sB)
+        tempObs.append(tempB)
+    name = "measurement_"+enviro[enviro.find("_")+1: enviro.find(".")] + ".txt"
+    file = open(name, "w")
+    file.write(str(gt[0][0])+" "+str(gt[0][1])+" "+str(gt[0][2])+"\n")
+    file.write(str(len(gt))+"\n")
+    for i in range(len(tempMake)):
+        file.write(str(tempMake[0][0]) + " " + str(tempMake[0][1]) + " " + str(tempMake[0][2]) + "\n")
+        for j in range(len(tempObs[i])):
+            file.write(str(tempObs[i][j]))
+            if(j==(len(tempObs[i])-1)):
+                file.write("\n")
+            else:
+                file.write(" ")
+    file.close()
+    return
+
 def drawEnv(coords, env, truth):
     fig = plt.figure()
     axis = fig.gca()
@@ -84,3 +123,6 @@ def drawEnv(coords, env, truth):
 env = readEnv("landmark_2.txt")
 truth = readGroundTruth("ground_truth_2.txt")
 drawEnv((50,50,0), env, truth)
+
+
+#sample("landmark_1.txt","ground_truth_1.txt")
